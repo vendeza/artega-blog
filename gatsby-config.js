def815da66faa9b6f16b51f6ee6f2cae436e61fd
artem <artm.avanesyan@gmail.com>
2020-06-04 15:31:22 +0300

    fix - remove site map plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-sitemap`,
+    // `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
